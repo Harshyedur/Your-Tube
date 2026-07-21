a3e8336dfe3aa25b4d114ac86e976538dc4cf2c1
Harshyedur <harshyedur@gmail.com>
2026-07-22 01:22:43 +0530

    update firebase config to own project

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyxbdclt2ocA5zgE-MDy1ndYIFqVMAr30",
-  authDomain: "yourtube-8cda9.firebaseapp.com",
-  projectId: "yourtube-8cda9",
-  storageBucket: "yourtube-8cda9.firebasestorage.app",
-  messagingSenderId: "921641878423",
-  appId: "1:921641878423:web:0d65801eebaf2b25f03ad2",
+  apiKey: "AIzaSyB3AeuhgXJwOJbbCvpDmtZihBGq6QkP04o",
+  authDomain: "your-tube-harsh.firebaseapp.com",
+  projectId: "your-tube-harsh",
+  storageBucket: "your-tube-harsh.firebasestorage.app",
+  messagingSenderId: "183887531020",
+  appId: "1:183887531020:web:2696fabc1386772b2f7229",
+  measurementId: "G-L7Y870ZJ7L",
 };
 
 // Initialize Firebase
